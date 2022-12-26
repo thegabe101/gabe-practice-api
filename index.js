54885933063c41eeeb0d1c271765641a01439757
Rayncor <sowa332@gmail.com>
2022-12-25 17:55:16 -0800

    scraped for musk

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
 const PORT = 8000;
 const express = require('express');
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.json('Welcome to the EV API.');
+});
+
+const articles = [];
+
+//this url will scrape from electrek
+app.get('/news', (req, res) => {
+    axios.get('https://electrek.co/').then((response) => {
+        //response.data here is the entire html block for the elektrek website 
+        const html = response.data;
+        //console.log(html);
+        const $ = cheerio.load(html);
+
+        $('a:contains("Musk")', html).each(function () {
+            const title = $(this).text();
+            const url = $(this).attr('href');
+            articles.push(title, url);
+        })
+        console.log(articles);
+    });
+});
+
+app.listen(PORT, () => console.log(`Server is online. Port ${PORT}.`));
 
